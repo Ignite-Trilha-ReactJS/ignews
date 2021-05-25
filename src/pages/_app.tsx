c9c00c5576ee783da2411b8c85bd893bb75a3ab3
Douglas Santos <douglas_motasts@outlook.com>
2021-05-25 18:07:46 -0300

    adicionando component Header

---
+++ src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import { AppProps } from 'next/app'
+
+import "../styles/global.scss";
+
+import Header from '../Components/Header';
+
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp
