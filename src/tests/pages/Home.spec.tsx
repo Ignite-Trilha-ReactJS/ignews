6c23f83c6f9247d1c941b568ac6745bf9c2a1c07
Douglas Gabriel  Mota dos Santos <douglasgmsantos@Air-de-Douglas.local>
2022-06-21 21:28:53 -0300

    test: Adicionando novos testes nas pages

--- src/tests/pages/Home.spec.tsx
+++ src/tests/pages/Home.spec.tsx
@@ -1,6 +1,10 @@
 import { render, screen } from '@testing-library/react'
-import Home from '../../pages'
+import { mocked } from 'ts-jest/utils'
 
+import { stripe } from '../../services/stripe'
+import Home, { getStaticProps } from '../../pages'
+
+jest.mock('../../services/stripe')
 jest.mock('next/dist/client/router')
 jest.mock('next-auth/client', () => {
     return {
@@ -16,5 +20,28 @@ describe("Home page", () => {
         render(<Home product={{ priceId: "fake-price-id", amount: "R$ 100,00" }}  />)
 
         expect(screen.getByText("R$ 100,00")).toBeInTheDocument();
+    })
+
+    it("loads initial data", async () => {
+        const retrivieStripePricesMocked = mocked(stripe.prices.retrieve)
+
+        retrivieStripePricesMocked.mockResolvedValueOnce({
+            id: "fake-price-id",
+            unit_amount: 1000
+        } as any)
+
+        const response = await getStaticProps({})
+
+        expect(response).toEqual(
+            expect.objectContaining({
+                props: {
+                    product: {
+                        amount : "$10.00",
+                        priceId: "fake-price-id"
+                    }
+                }
+            })
+        )
+
     })
 })
